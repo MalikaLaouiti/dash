a5503d24909aa0736ed5c20776fc961668a4ce2a
MalikaLaouiti <laouiti.malika@yahoo.com>
2025-10-23 15:17:31 +0100

    removing useless tab in table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -127,9 +127,7 @@ export default function Home() {
                 <TabsTrigger value="supervisors-professional">
                   Encadreurs Professionnels ({parsedData?.summary.totalSupervisors.professionnels || 0})
                 </TabsTrigger>
-                {/* <TabsTrigger value="raw">
-                  Vue combinée ({filteredData.length})
-                </TabsTrigger> */}
+                
                 <TabsTrigger value="json">
                   JSON
                 </TabsTrigger>
@@ -150,10 +148,6 @@ export default function Home() {
               <TabsContent value="supervisors-professional" className="mt-6">
                 <DataTable data={parsedData} activeTab="supervisors-professional" selectedYear={selectedYear || undefined} />
               </TabsContent>
-
-              {/* <TabsContent value="raw" className="mt-6">
-                <DataTable data={parsedData} activeTab="raw" selectedYear={selectedYear || undefined} />
-              </TabsContent> */}
 
               <TabsContent value="json" className="mt-6">
                 <JsonPreview data={parsedData} />
