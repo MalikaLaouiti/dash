37e19e0af2af5275ed088783d7f6f0f6f2c51c12
MalikaLaouiti <laouiti.malika@yahoo.com>
2026-01-15 18:17:47 +0100

    creating supervisor analytic page

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,21 +2,9 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { useState } from "react";
-import { ExcelParser, type ParsedExcelData } from "@/lib/excel-parser";
-import { useData } from "@/Context/DataContext";
 
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-  const [searchQuery, setSearchQuery] = useState<string>("")
-  const [searchFilters, setSearchFilters] = useState({
-    students: true,
-    companies: true,
-    supervisors: true,
-  })
-  
- 
-  
-  // const filteredData = parsedData ? ExcelParser.searchData(parsedData, searchQuery, searchFilters, selectedYear || undefined) : [];
   return (
     <SidebarProvider>
       <div className="flex h-screen w-full bg-background">
